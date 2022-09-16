b4c53604016ca86ac7fb1b143c6336dbc7de250a
paezn <npaez@colegioguanenta.edu.co>
2022-09-16 07:16:38 -0500

    Se agrega el frame operaciones

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ## Ventana principal y frame entrada datos
 
 ![ventana principal y frame entrada](ventana_principal.png "Ventana principal y frame entrada")
+
+## frame operaciones (botones)
+
+![frame operaciones](frame_operaciones.png "frame operaciones")
--- gui_01/gui_01.py
+++ gui_01/gui_01.py
@@ -6,7 +6,6 @@ from tkinter import messagebox
 # funciones de la app
 # --------------------
 
-"""
 def sumar():
     # messagebox.showinfo("Suma 1.0", "Hizo click en el boton sumar...")
     c = int(a.get()) + int(b.get())
@@ -20,7 +19,7 @@ def borrar():
 
 def salir():
     messagebox.showinfo("Suma 1.0", "La app se cerrará...")
-    ventana_principal.destroy()"""
+    ventana_principal.destroy()
 
 # ------------------
 # ventana principal
@@ -99,7 +98,7 @@ etiq_b.place(x=585, y= 120)
 entry_b = Entry(frame_entrada, width=4, textvariable=b)
 entry_b.config(font=("Arial", 20))
 entry_b.place(x=682,y=120)
-"""
+
 # ------------------
 # frame operaciones
 # ------------------
@@ -108,23 +107,23 @@ frame_operaciones.config(bg="ivory2", width=780, height=120)
 frame_operaciones.place(x=10,y=260)
 
 # boton para sumar los números - texto
-bt_sum = PhotoImage(file="img/boton_sumar.png")
+bt_sum = PhotoImage(file="gui_01/img/boton_sumar.png")
 # bt_sumar = Button(frame_operaciones, text= "Sumar", width=10)
 bt_sumar = Button(frame_operaciones, image=bt_sum, width=105, height=105, command=sumar)
 bt_sumar.place(x=116, y=7)
 
 # boton para borrar entradas y resultado
-bt_bor = PhotoImage(file="img/boton_borrar.png")
+bt_bor = PhotoImage(file="gui_01/img/boton_borrar.png")
 # bt_borrar = Button(frame_operaciones, text="Borrar", width=10)
 bt_borrar = Button(frame_operaciones, image=bt_bor, width=105, height=105, command=borrar)
 bt_borrar.place(x=337, y=7)
 
 # boton para salir - cerrar la app
-bt_sal = PhotoImage(file="img/boton_salir.png")
+bt_sal = PhotoImage(file="gui_01/img/boton_salir.png")
 # bt_salir = Button(frame_operaciones, text="Salir", width=10)
 bt_salir = Button(frame_operaciones, image=bt_sal, width=105, height=105, command=salir)
 bt_salir.place(x=558, y=7)
-
+"""
 # ------------------
 # frame resultados
 # ------------------
